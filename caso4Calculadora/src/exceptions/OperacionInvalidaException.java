5339b8e7401f1147fb74f1a3402dc0bcec96db9f
Franco Ercoli <89115165+francoercoli@users.noreply.github.com>
2021-09-01 19:18:07 -0300

    Delete caso4Calculadora directory

--- caso4Calculadora/src/exceptions/OperacionInvalidaException.java
+++
@@ -1,6 +0,0 @@
-package exceptions;
-
-@SuppressWarnings("serial")
-public class OperacionInvalidaException extends Exception {
-
-}
--- caso4Calculadora/src/vista/IVista.java
+++
@@ -1,9 +0,0 @@
-package vista;
-
-public interface IVista {
-
-	public void Calcular(int PrimerOperando,int SegundoOperando,String Operacion);
-	public String mostrarEstado(String Mensaje);
-	public int MostrarResultado();
-	
-}
