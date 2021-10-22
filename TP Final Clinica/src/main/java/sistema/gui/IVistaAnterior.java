b073a722291e7fca0bb03a935b91d6234225209f
JuanCruzMateos <juanczmt@gmail.com>
2021-10-22 11:08:31 -0300

    vista ant

--- TP Final Clinica/src/main/java/sistema/gui/IVistaAnterior.java
+++ TP Final Clinica/src/main/java/sistema/gui/IVistaAnterior.java
@@ -1,14 +1,17 @@
 package sistema.gui;
 
-import sistema.habitaciones.Habitacion;
-import sistema.personas.medicos.IMedico;
-import sistema.personas.pacientes.Paciente;
-
 import java.awt.event.ActionListener;
 import java.awt.event.WindowListener;
 import java.util.Iterator;
 
-public interface IVista {
+import javax.swing.JTextPane;
+
+import sistema.habitaciones.Habitacion;
+import sistema.personas.medicos.IMedico;
+import sistema.personas.pacientes.Asociado;
+import sistema.personas.pacientes.Paciente;
+
+public interface IVistaAnterior {
     //Facturacion
     Paciente getPacienteFacturacion();
 
@@ -27,6 +30,30 @@ public interface IVista {
     void actualizarComboHabitacionesFacturacion(Iterator<Habitacion> iterator);
 
     void MostrarFactura(String detalle);
+
+    //ALTA/BAJA
+    void actualizarListaAsociados(Iterator<Asociado> iterator);
+
+    String getNombreAsociado();
+
+    String getApellidoAsociado();
+
+    int getDNIAsociado();
+
+    String getDireccionAsociado();
+
+    Long getTelefonoAsociado();
+
+    int getDNIAsociadoAEliminar();
+
+    //SIMULACION
+    Asociado getAsociadoSimulacion();
+
+    int getCantidadSolicitudesAsociado();
+
+    int getCantidadSolicitudesOperario();
+
+    JTextPane getTextPane();
 
     void addActionListener(ActionListener actionListener);
 
